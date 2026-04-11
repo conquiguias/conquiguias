04c04267f5653391bf043bda6ee5038e9dea88cf
PROYECTO JA <kendall.torres.17@gmail.com>
2026-04-11 02:14:12 -0600

    aaaaa

---
+++ commit_changes.ps1
@@ -0,0 +1,4 @@
+cd "c:\Users\PROYECTO JA\Documents\GitHub\conquiguias"
+git add panel.html
+git commit -m "Allow all users to attempt cloud operations; let API handle permissions"
+git push origin main
